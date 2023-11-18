843b2e26920a2bc4df75c251225c5fba6a205d27
Luid <eng.limaluis@gmail.com>
2023-11-17 23:13:19 -0300

    switch radius to diameter in maths(and try to calculete the ceiling of the price)

--- webpage/orcamento.py
+++ webpage/orcamento.py
@@ -57,18 +57,19 @@ def obter_peso_gramas(material,volume):
 def calcula_volume_retangulo(comprimento, largura, altura):
     return comprimento * largura * altura
 
-def calcula_volume_cilindro(raio, altura):
-    return math.pi * raio**2 * altura
+def calcula_volume_cilindro(diametro, altura):
+    return math.pi * (diametro/2)**2 * altura
 
-def calcula_volume_circulo(raio):
-    return math.pi * raio**2
+def calcula_volume_circulo(diametro):
+    return math.pi * (diametro/2)**2
 
 def main():
     st.title("Orçamento para Impressão 3D")
 
     # Seleção da impressora
 
-    impressora_selecionada = st.selectbox("Selecione a impressora:", ["Ender 3", "Ender 3 S1", "Prusa MK4", "Ender 5"])
+    impressora_selecionada = st.selectbox("Selecione a impressora:", ["Ender 3", "Ender 3 S1",
+                                                                      "Prusa MK4", "Ender 5"])
 
     # Seleção do material
 
@@ -76,7 +77,8 @@ def main():
 
     # Escolha da forma (retângulo, cilindro, círculo ou ambos)
 
-    forma_selecionada = st.radio("Selecione a forma:", ["Retângulo", "Cilindro", "Círculo", "Ambos"])
+    forma_selecionada = st.radio("Selecione a forma:", ["Retângulo", "Cilindro", "Círculo",
+                                                        "Varias"])
 
     if forma_selecionada == "Ambos":
         st.subheader("Medidas para o Retângulo:")
@@ -85,15 +87,15 @@ def main():
         altura_retangulo = st.number_input("Altura (cm):", min_value=0.1, step=0.1)
 
         st.subheader("Medidas para o Cilindro:")
-        raio_cilindro = st.number_input("Raio (cm):", min_value=0.1, step=0.1)
+        diametro_cilindro = st.number_input("Diametro (cm):", min_value=0.1, step=0.1)
         altura_cilindro = st.number_input("Altura (cm):", min_value=0.1, step=0.1)
 
         st.subheader("Medidas para o Círculo:")
-        raio_circulo = st.number_input("Raio (cm):", min_value=0.1, step=0.1)
+        diametro_circulo = st.number_input("Diametro (cm):", min_value=0.1, step=0.1)
 
         volume_retangulo = calcula_volume_retangulo(comprimento_retangulo, largura_retangulo, altura_retangulo)
-        volume_cilindro = calcula_volume_cilindro(raio_cilindro, altura_cilindro)
-        volume_circulo = calcula_volume_circulo(raio_circulo)
+        volume_cilindro = calcula_volume_cilindro(diametro_cilindro, altura_cilindro)
+        volume_circulo = calcula_volume_circulo(diametro_circulo)
 
         # Calcula o volume total considerando os três objetos
         volume_total = volume_retangulo + volume_cilindro + volume_circulo
@@ -105,26 +107,26 @@ def main():
         # Calcula o volume apenas para o retângulo
         volume_total = calcula_volume_retangulo(comprimento_retangulo, largura_retangulo, altura_retangulo)
     elif forma_selecionada == "Cilindro":
-        raio_cilindro = st.number_input("Raio (cm):", min_value=0.1, step=0.1)
+        diametro_cilindro = st.number_input("Diametro (cm):", min_value=0.1, step=0.1)
         altura_cilindro = st.number_input("Altura (cm):", min_value=0.1, step=0.1)
 
         # Calcula o volume apenas para o cilindro
-        volume_total = calcula_volume_cilindro(raio_cilindro, altura_cilindro)
+        volume_total = calcula_volume_cilindro(diametro_cilindro, altura_cilindro)
     else:
-        raio_circulo = st.number_input("Raio (cm):", min_value=0.1, step=0.1)
+        diametro_circulo = st.number_input("Diametro (cm):", min_value=0.1, step=0.1)
 
         # Calcula o volume apenas para o círculo
-        volume_total = calcula_volume_circulo(raio_circulo)
+        volume_total = calcula_volume_circulo(diametro_circulo)
 
     # Exibe o volume total calculado
-    st.subheader("Volume Total do Objeto:")
+    st.write("Volume Total do Objeto:")
     st.write(f"{volume_total:.2f} cm³")
 
     # Obtém a taxa da hora da máquina com base na impressora selecionada
     taxa_hora_maquina = obter_taxa_hora_maquina(impressora_selecionada)
 
     # Exibe a taxa da hora da máquina
-    st.subheader("Taxa da Hora da Máquina:")
+    st.write("Taxa da Hora da Máquina:")
     st.write(f"R$ {taxa_hora_maquina:.2f} por cm³")
 
     # Obtém o preço do material com base no material selecionado
@@ -134,14 +136,15 @@ def main():
     peso_em_gramas = obter_peso_gramas(material_selecionado,volume_total)
 
     # Exibe o preço do material
-    st.subheader("Preço do Material:")
+    st.write("Preço do Material:")
     st.write(f"R$ {preco_material:.2f} por cm³")
 
     # Calcula o preço com base no volume, taxa da hora da máquina e preço do material
     preco = calcula_orcamento(volume_total, taxa_hora_maquina, preco_material, peso_em_gramas)
 
+    preco = int(preco)+1
     # Exibe o preço calculado
-    st.subheader("Preço estimado:")
+    st.header("Preço estimado:")
     st.write(f"R$ {preco:.2f}")
 
 if __name__ == "__main__":
